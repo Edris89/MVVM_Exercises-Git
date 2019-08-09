f97a614049686a1ff1a4315fbc952b47261ac5d0
Edris89 <44404984+Edris89@users.noreply.github.com>
2019-08-10 00:11:25 +0200

    First Commit

---
+++ MyViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvm_exercise
+{
+    class MyViewModel : INotifyPropertyChanged
+    {
+        class User
+
+        {
+            private string firstname;
+            private string lastname;
+
+            public string FirstName
+            {
+                get { return firstname; }
+
+                set { firstname = value; }
+            }
+
+            public string LastName
+            {
+                get { return lastname; }
+
+                set { lastname = value; }
+            }
+
+        }
+
+        private User user;
+
+        public string FirstName
+        {
+            get { return user.FirstName; }
+            set
+            {
+                if (user.FirstName != value)
+                {
+                    user.FirstName = value;
+                    OnPropertyChange("FirstName");
+                    OnPropertyChange("FullName");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return user.LastName; }
+            set
+            {
+                if (user.LastName != value)
+                {
+                    user.LastName = value;
+                    OnPropertyChange("LastName");
+                    OnPropertyChange("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public MyViewModel()
+        {
+            user = new User
+            {
+                FirstName = "Edris",
+                LastName = "C# Beginner xD",
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
